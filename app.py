ccd609d9e43c567b1ce448d571d0534b503aea06
Calum Mackervoy <c.mackervoy@gmail.com>
2023-05-18 17:27:38 +0100

    feature: GET characters

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ import uuid
 from flask import Flask, request, jsonify
 #from rdflib import Graph
 from urllib.parse import unquote_plus
-#from pymongo import MongoClient
+from pymongo import MongoClient
 from bson import json_util
 
 # config
 
 app = Flask(__name__)
 
-#client = MongoClient('localhost', 27017)
-#db = client.situation
+client = MongoClient('localhost', 27017)
+db = client.situation
 
 @app.route("/")
 def main():
@@ -22,6 +22,19 @@ def main():
 def character_templates():
     characters = list(db.character_templates.find({"@type": "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mudchar.ttl#Character"}))
     return jsonify(json.loads(json_util.dumps(characters))), 200, {'Content-Type': 'application/ld+json'}
+'''
+
+'''
+REST endpoints for characters, cards, events, actions
+'''
+
+@app.route("/characters/", methods=['GET'])
+def characters():
+    characters = list(db.characters.find({"@type": "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mudchar.ttl#Character"}))
+    return jsonify(json.loads(json_util.dumps(characters))), 200, {'Content-Type': 'application/ld+json'}
+
+'''
+Routes for supporting complex behaviour in cards
 '''
 
 def _get_target_obj(world_data, action_data):
