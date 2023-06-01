d4e4e4347b9a703997d9216e4628c3f6eea296f4
Calum Mackervoy <c.mackervoy@gmail.com>
2023-06-01 13:59:46 +0100

    refactor (utils): moved some util functions into new file

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from PIL import Image
 from pymongo import MongoClient
 from bson import json_util
 from mud.vocab import MUD_CHAR, MUD_DIALOGUE
+from mud.utils import get_target_obj, get_recorded_history_for_event, prepare_action_changes
 
 # config
 
@@ -315,59 +316,6 @@ def generate_context():
 Routes for supporting complex behaviour in cards
 '''
 
-def _get_target_obj(world_data, action_data):
-    '''
-    gets the target object based on the action and the world data
-    '''
-    # current solution exploits implicit knowledge of the ontologies used in the game
-    # TODO: consider using shapes or SPARQL for this, searching the object. GraphQL...?
-    def _find_region_with_urlid(world_data, urlid):
-        if world_data["@id"] == urlid:
-            return world_data
-        elif "mudworld:hasRegions" in world_data:
-            for region in world_data["mudworld:hasRegions"]:
-                res = _find_region_with_urlid(region, urlid)
-                if res is not None:
-                    return res
-        return None
-
-    target_id = action_data["mudcard:playTarget"]["@id"] if "@id" in action_data["mudcard:playTarget"] else None
-    target_type = action_data["mudcard:playTarget"]["@type"]
-
-    if target_type == "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mudworld.ttl#Region":
-        if target_id is not None:
-            return _find_region_with_urlid(world_data, target_id)
-        else:
-            # TODO: allow for searching with shapes or SPARQL or owt, for less specific query
-            pass
-
-def _get_recorded_history_for_event(event_text):
-    return {
-        "@id": "_:RecordedHistory_" + str(uuid.uuid4()),
-        "@type": "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/games/twt2023.ttl#RecordedHistory",
-        "n:hasNote": event_text
-    }
-
-def _prepare_action_changes(action_data, deletes=[], inserts=[]):
-    return {
-        "@context": {
-            "n": "http://www.w3.org/2006/vcard/ns#",
-            "mud": "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mud#",
-            "mudchar": "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mudchar.ttl#",
-            "mudcontent": "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mudcontent.ttl#",
-            "mudlogic": "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mudlogic.ttl#",
-            "mudcard": "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mudcard.ttl#"
-        },
-        "@id": action_data["@id"],
-        "@type": action_data["@type"], 
-        "mudlogic:patchesOnComplete": {
-            "@id": "_:endState",
-            "@type": "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mudlogic.ttl#Patch",
-            "mudlogic:deletes": deletes,
-            "mudlogic:inserts": inserts
-        }
-    }
-
 # TWT game jam endpoints
 # the data passed isn't negotiated for now, it's always the full world data of the battle
 # later we will want to support GET so that we can negotiate it using shapes
@@ -381,7 +329,7 @@ def bog_monster_eats_villager():
     action_data = data["actionData"]
     actor_data = data["actorData"]
 
-    target = _get_target_obj(world_data, action_data)
+    target = get_target_obj(world_data, action_data)
     result = {}
 
     # eat the first villager you find
@@ -395,9 +343,9 @@ def bog_monster_eats_villager():
                     message = f"{actor_data['n:fn']} ate a villager from the region {target['n:fn']}"
                 inserts = [
                     pop,
-                    _get_recorded_history_for_event(message)
+                    get_recorded_history_for_event(message)
                 ]
-                result = _prepare_action_changes(action_data, [], inserts)
+                result = prepare_action_changes(action_data, [], inserts)
                 break
 
     return jsonify(result), 200, _get_headers({'Content-Type': 'application/ld+json'})
