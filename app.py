5291109f7b804e6f0ad882cfb5f31f54bb15d077
Calum Mackervoy <c.mackervoy@gmail.com>
2023-06-05 12:25:38 +0100

    logs

--- app.py
+++ app.py
@@ -314,7 +314,9 @@ def generate_context():
             validate_result, report, message = validate(world_graph, shape_graph=shape_graph, inference="none", debug=True)
             if validate_result:
                 print(str(candidate_obj["@id"]) + " passed on shape (" + str(validate_result) + ")")
-                print(str(shape))
+                print(str(world_graph.serialize("json-ld")))
+                print("\n\n")
+                print(str(shape_graph.serialize("json-ld")))
                 selected_candidate = candidate_obj
                 if "muddialogue:bindingIsUnique" in binding and binding["muddialogue:bindingIsUnique"]:
                     exclude_candidates.append(candidate_obj["@id"])
