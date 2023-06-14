24c794817b37e2a003effd45d43906e8a64745e9
Calum Mackervoy <c.mackervoy@gmail.com>
2023-06-14 16:40:06 +0200

    feature (API): added world_templates collection

--- app.py
+++ app.py
@@ -249,6 +249,54 @@ def cards():
     cards = list(db.cards.find({"@type": "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mudchar.ttl#Character"}))
     return jsonify(json.loads(json_util.dumps(cards))), 200, _get_headers({'Content-Type': 'application/ld+json'})
 
+@app.route("/worlds/templates/<world_id>/", methods=['GET'])
+def world_templates_detail(world_id):
+    w = db.world_templates.find_one({"@id": f"{site_url}/worlds/templates/{world_id}/"})
+    if w is None:
+        return "world with this urlid not found", 404
+    return jsonify(json.loads(json_util.dumps(w))), 200, _get_headers({'Content-Type': 'application/ld+json'})
+
+@app.route("worlds/templates/", methods=['GET', 'POST', 'DELETE', 'OPTIONS'])
+def world_templates():
+    if request.method == 'OPTIONS':
+        return _get_default_options_response(request)
+    
+    if request.method == 'POST':
+        jsonld = copy.deepcopy(request.get_json())
+
+        if "_id" in jsonld:
+            jsonld.pop("_id")
+
+        if "@type" not in jsonld or len(jsonld["@type"]) == 0:
+            jsonld["@type"] = MUD_WORLD.Region
+
+        if "@id" not in jsonld or len(jsonld["@id"]) == 0:
+            jsonld["@id"] = f"{site_url}/worlds/templates/{str(uuid.uuid4())}/"
+
+        db.world_templates.find_one_and_replace(
+            {"@id": jsonld["@id"]},
+            jsonld,
+            upsert=True
+        )
+
+        return jsonify(jsonld), 201, _get_headers({'Content-Type': 'application/ld+json'})
+
+    if request.method == 'DELETE':
+        jsonld = request.get_json()
+
+        if "@id" not in jsonld:
+            return "@id key is required for DELETE", 400
+        
+        val = db.world_templates.find_one_and_delete({"@id": jsonld["@id"]})
+
+        if val is None:
+            return "", 404, _get_headers()
+
+        return "", 204, _get_headers()
+
+    world = list(db.world_templates.find({"@type": MUD_WORLD.Region}))
+    return jsonify(json.loads(json_util.dumps(world))), 200, _get_headers({'Content-Type': 'application/ld+json'})
+
 @app.route("/worlds/<world_id>/", methods=['GET'])
 def world_detail(world_id):
     w = db.worlds.find_one({"@id": f"{site_url}/worlds/{world_id}/"})
@@ -271,7 +319,7 @@ def worlds():
             jsonld["@type"] = MUD_WORLD.Region
 
         if "@id" not in jsonld or len(jsonld["@id"]) == 0:
-            jsonld["@id"] = f"{site_url}/world/{str(uuid.uuid4())}/"
+            jsonld["@id"] = f"{site_url}/worlds/{str(uuid.uuid4())}/"
 
         db.worlds.find_one_and_replace(
             {"@id": jsonld["@id"]},
