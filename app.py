578e726d1720a9da1ba226b910e652ba81f4a620
Calum Mackervoy <c.mackervoy@gmail.com>
2024-02-04 07:31:31 +0100

    update: removed old game jam content from server

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import json
 import copy
 import uuid
 from flask import request, jsonify, send_file
-from mud.utils import get_target_obj, get_recorded_history_for_event, prepare_action_changes
 from config import app
 from view_utils import get_headers, get_default_options_response
 from users import users_blueprint
@@ -58,41 +57,3 @@ def scene_description():
             "mudcontent:hasImage": "https://example.com/queenAnnesRevenge/"
         }
     }), 200, get_headers({'Content-Type': 'application/ld+json'})
-
-'''
-Routes for supporting complex behaviour in cards
-'''
-
-# TWT game jam endpoints
-# the data passed isn't negotiated for now, it's always the full world data of the battle
-# later we will want to support GET so that we can negotiate it using shapes
-@app.route("/bogMonsterEatsVillager/", methods=["POST", "OPTIONS"])
-def bog_monster_eats_villager():
-    if request.method == 'OPTIONS':
-        return get_default_options_response(request)
-
-    data = request.get_json()
-    world_data = data["worldData"]
-    action_data = data["actionData"]
-    actor_data = data["actorData"]
-
-    target = get_target_obj(world_data, action_data)
-    result = {}
-
-    # eat the first villager you find
-    if "mudworld:hasPopulations" in target:
-        for pop in target["mudworld:hasPopulations"]:
-            if "mudworld:populationNumber" in pop and pop["mudworld:populationNumber"] > 0 and "mud:species" in pop and pop["mud:species"]["@id"] == "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mud.ttl#Human":
-                pop["mudworld:populationNumber"] = pop["mudworld:populationNumber"] - 1
-                if pop["mudworld:populationNumber"] == 0:
-                    message = f"{actor_data['n:fn']} ate the last villager in the region {target['n:fn']}, now they are extinct!"
-                else:
-                    message = f"{actor_data['n:fn']} ate a villager from the region {target['n:fn']}"
-                inserts = [
-                    pop,
-                    get_recorded_history_for_event(message)
-                ]
-                result = prepare_action_changes(action_data, [], inserts)
-                break
-
-    return jsonify(result), 200, get_headers({'Content-Type': 'application/ld+json'})
